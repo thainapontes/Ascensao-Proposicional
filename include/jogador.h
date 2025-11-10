edd30b257bda8fe880c41c94ab44a200f9e7a05a
Thainá Pontes <thainapontes14@gmail.com>
2025-11-10 11:20:40 -0300

    Define Jogador struct and declare functions
    Added struct definition and function prototypes for Jogador.

--- include/jogador.h
+++ include/jogador.h
@@ -2,3 +2,20 @@
 #define __JOGADOR_H__
 
 #include "tipos.h"
+
+// definiçao struct jogador
+struct Jogador {
+    int x; // Posição horizontal na tela
+    int y; // Posição vertical na tela
+    int velocidade_y; //  Campo para simulação de física (velocidade vertical)
+    int pontuacao;
+    int vidas;
+    char simbolo; 
+};
+
+// apenas o prototipo das funções, para a maquina saber  q elas existem
+void Jogador_Inicializar(Jogador *jogador);
+void Jogador_Atualizar(Jogador *jogador, Plataforma *plataformas);
+void Jogador_MoverHorizontal(Jogador *jogador, int direcao);
+
+#endif // __JOGADOR_H__
