6df29daf1973b223c433527171b85977f3bde7fa
Thainá Pontes <thainapontes14@gmail.com>
2025-11-11 11:01:55 -0300

    Update jogador.h

--- include/jogador.h
+++ include/jogador.h
@@ -15,7 +15,7 @@ struct Jogador {
 
 // apenas o prototipo das funções, para a maquina saber  q elas existem
 void Jogador_Inicializar(Jogador *jogador);
-void Jogador_Atualizar(Jogador *jogador, Plataforma *plataformas);
+void Jogador_Atualizar(Jogador *j, Plataforma plataformas[], int num_plataformas);
 void Jogador_MoverHorizontal(Jogador *jogador, int direcao);
 long long Jogador_CalcularFatorial(int n); //RECURSIVIDADE: Calcula o fatorial de um número para pontuação bônus.
 
