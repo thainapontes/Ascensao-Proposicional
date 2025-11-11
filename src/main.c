7bfb3878142099a7b0c5f01a15979e5fd655b77b
Thainá Pontes <thainapontes14@gmail.com>
2025-11-10 22:43:09 -0300

    Implement player symbol selection and game setup
    Added player symbol input and initialization logic.

--- src/main.c
+++ src/main.c
@@ -6,6 +6,24 @@
 #include "visual.h"
 
 int main() {
+    
+    Jogador meu_jogador;
+    char simbolo_escolhido;
+
+    // FASE 1: Coleta de Dados (Antes de inicializar o modo gráfico)
+    printf("--- BEM-VINDO(A) A POUPROSIÇÕES ---\n");
+    printf(" Voc
+    printf("Digite o caractere que irá representar seu Jogador (ex: @, P, J): ");
+    
+    // Lê o caractere escolhido pelo usuário
+    if (scanf(" %c", &simbolo_escolhido) != 1) {
+        simbolo_escolhido = '@'; // Fallback: usa '@' se a leitura falhar
+    }
+
+    // ----------------------------------------------------
+    // FASE 2: Inicialização do Jogo e do Modo Gráfico
+    // ----------------------------------------------------
+    
     screenInit(1); // inicializar a tela e desenha as bordas
     keyboardInit(); // configura ro teclado para leitura inst
     timerInit(50); // inicializar o timer 
@@ -31,6 +49,12 @@ int main() {
         if (timerTimeOver()) {
             // Lógica para mover o aluno para baixo (Gravidade)
             // Lógica para desenhar/atualizar o estado da tela (screenUpdate)
+            
+            // Dentro do main.c, quando a pergunta aparece:
+            Visual_DesenharGuilherme(5, 5); // Desenha o ícone no canto superior esquerdo
+            
+            // Dentro do loop de desenho:
+            Visual_DesenharDiego(SCRENDX - 7, SCRENDY - 3); // Desenha o ícone no canto inferior direito
         }
 
         // CONDIÇÃO DE FIM DE JOGO VIRÁ AQUI
