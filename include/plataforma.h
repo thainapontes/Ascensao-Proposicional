d2d2989cd4bef7c80f1f8298c7fb2613aa38dbda
Thainá Pontes <thainapontes14@gmail.com>
2025-11-11 08:43:58 -0300

    Add Plataforma struct and function declarations
    Define the Plataforma struct and related functions.

--- include/plataforma.h
+++ include/plataforma.h
@@ -1,1 +1,18 @@
+#ifndef __PLATAFORMA_H__
+#define __PLATAFORMA_H__
+#include "tipos.h" 
 
+struct Plataforma {
+    int x;
+    int y;
+    int largura;
+    TipoPlataforma tipo; // Usa o enum definido em tipos.h
+    int usada;           // Flag para plataformas quebráveis
+};
+
+
+void Plataforma_GerarNova(Plataforma *p, int y_inicial);
+void Plataforma_Desenhar(Plataforma *p);
+int Plataforma_VerificarColisao(Jogador *jogador, Plataforma *p);
+
+#endif 
