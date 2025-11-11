626eb4813e1e58a1b631e384285e2642c945464b
Thainá Pontes <thainapontes14@gmail.com>
2025-11-11 10:48:14 -0300

    Add Jogador_CalcularFatorial function prototype
    Added a function prototype for calculating the factorial of a number.

--- include/jogador.h
+++ include/jogador.h
@@ -17,5 +17,6 @@ struct Jogador {
 void Jogador_Inicializar(Jogador *jogador);
 void Jogador_Atualizar(Jogador *jogador, Plataforma *plataformas);
 void Jogador_MoverHorizontal(Jogador *jogador, int direcao);
+long long Jogador_CalcularFatorial(int n); //RECURSIVIDADE: Calcula o fatorial de um número para pontuação bônus.
 
-#endif // __JOGADOR_H__
+#endif 
